5c64295f48a04d8ac5ec122eba2d37ec59199c42
ByCamilleLab <camillejean.15100@gmail.com>
2026-01-20 20:13:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.produit-card');
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.style.opacity = "1";
-                entry.target.style.transform = "translateY(0)";
-            }
-        });
-    }, { threshold: 0.1 });
 
-    cards.forEach(card => observer.observe(card));
-});
